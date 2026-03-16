d6fa57bc77acd5109f249bf1f788d5d638607f21
tratchapong <tratchapong@gmail.com>
2026-03-16 11:41:57 +0700

    Register v2 : fetch api

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { registerSchema } from "@/validations/schema"
+import axios from "axios"
 
 
 function RegisterForm() {
@@ -15,10 +16,12 @@ function RegisterForm() {
 
   const onSubmit = async (data) => {
     try {
-      alert(JSON.stringify(data, null, 2))
+      // alert(JSON.stringify(data, null, 2))
+      const resp = await axios.post('http://localhost:8899/api/auth/register', data)
+      alert(JSON.stringify(resp,null,2))
     } catch (err) {
-      // alert(JSON.stringify(err, null, 2))
-      console.log(errors)
+      console.log(err)
+      alert(JSON.stringify(err,null,2))
     }
   }
   return (
@@ -29,35 +32,49 @@ function RegisterForm() {
         className='flex flex-col gap-5 p-4 pt-3'
       >
         <div className="flex gap-2">
-          <input type="text" className="input w-full"
-            placeholder='First name'
-            {...register('firstName')}
-          />
-          <input type="text" className="input w-full"
-            placeholder='Last name'
-            {...register('lastName')}
-          />
+          <div className="w-full">
+            <input type="text" className="input w-full"
+              placeholder='First name'
+              {...register('firstName')}
+            />
+            <p className="text-sm text-error">{errors.firstName?.message}</p>
+          </div>
+          <div className="w-full">
+            <input type="text" className="input w-full"
+              placeholder='Last name'
+              {...register('lastName')}
+            />
+            <p className="text-sm text-error">{errors.lastName?.message}</p>
+          </div>
         </div>
-        <input type="text" className="input w-full"
-          placeholder='Email or Phone number'
-          {...register('identity')}
-        />
-        <input type="password" className="input w-full"
-          placeholder='New password'
-          {...register('password')}
-        />
-        <input type="password" className="input w-full"
-          placeholder='Confirm password'
-          {...register('confirmPassword')}
-        />
+        <div className="w-full">
+          <input type="text" className="input w-full"
+            placeholder='Email or Phone number'
+            {...register('identity')}
+          />
+          <p className="text-sm text-error">{errors.identity?.message}</p>
+        </div>
+        <div className="w-full">
+          <input type="password" className="input w-full"
+            placeholder='New password'
+            {...register('password')}
+          />
+          <p className="text-sm text-error">{errors.password?.message}</p>
+        </div>
+        <div className="w-full">
+          <input type="password" className="input w-full"
+            placeholder='Confirm password'
+            {...register('confirmPassword')}
+          />
+          <p className="text-sm text-error">{errors.confirmPassword?.message}</p>
+        </div>
         <button className="btn btn-secondary text-xl">Sign up</button>
       </form>
-
       <div className="border">
         <pre className="text-error text-xs">
-          {JSON.stringify(errors, (k, v) => k === 'ref' ? undefined : v, 2)}</pre>
+          {JSON.stringify(errors, (k, v) => k === 'ref' ? undefined : v, 2)}
+        </pre>
       </div>
-
     </>
   )
 }
