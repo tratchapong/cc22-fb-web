01f83dc84eaf35444baff0983a56c650360579f0
tratchapong <tratchapong@gmail.com>
2026-03-17 15:28:37 +0700

    Create Post + PostForm modal

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,36 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Avatar from './Avatar'
 import useUserStore from '@/stores/userStore'
 import { ActivityIcon, PhotoIcon, VideoIcon } from '@/icons'
+import PostForm from './PostForm'
 
 function CreatePost() {
   const user = useUserStore(state => state.user)
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect( ()=>{
+    if(isOpen) {
+      document.getElementById('postform').showModal()
+    }else{
+      document.getElementById('postform').close()
+    }
+  },[isOpen])
   return (
-    <div className='card bg-base-100 shadow-xl'>
-      <div className="card-body p-6">
-        <div className="flex gap-2">
-          <Avatar className='w-11 h-11 rounded-full' imgSrc={user.profileImage}/>
-          <button className="btn flex-1 rounded-full justify-start">
-            What do you think? {user.firstName}
-          </button>
-        </div>
-        <div className="divider mt-1 mb-0"></div>
-        <div className="flex gap-3 justify-between *:flex-1 *:cursor-pointer">
-          <div className="flex gap-3 justify-center py-2 rounded-lg hover:bg-gray-300">
-            <VideoIcon className='w-6' />
-            Live /Stream
+    <>
+      <div className='card bg-base-100 shadow-xl'>
+        <div className="card-body p-6">
+          <div className="flex gap-2">
+            <Avatar className='w-11 h-11 rounded-full' imgSrc={user.profileImage} />
+            <button className="btn flex-1 rounded-full justify-start"
+              onClick={()=>setIsOpen(true)}
+            >
+              What do you think? {user.firstName}
+            </button>
           </div>
-          <div className="flex gap-3 justify-center py-2 rounded-lg hover:bg-gray-300">
-            <PhotoIcon className='w-6' />
-            Photo /Video
-          </div>
-          <div className="flex gap-3 justify-center py-2 rounded-lg hover:bg-gray-300">
-            <ActivityIcon className='w-6' />
-            Activity
+          <div className="divider mt-1 mb-0"></div>
+          <div className="flex gap-3 justify-between *:flex-1 *:cursor-pointer">
+            <div className="flex gap-3 justify-center py-2 rounded-lg hover:bg-gray-300">
+              <VideoIcon className='w-6' />
+              Live /Stream
+            </div>
+            <div className="flex gap-3 justify-center py-2 rounded-lg hover:bg-gray-300">
+              <PhotoIcon className='w-6' />
+              Photo /Video
+            </div>
+            <div className="flex gap-3 justify-center py-2 rounded-lg hover:bg-gray-300">
+              <ActivityIcon className='w-6' />
+              Activity
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <dialog id="postform" className="modal" onClose={()=>setIsOpen(false)}>
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          </form>
+          {isOpen && <PostForm />}
+        </div>
+      </dialog>
+    </>
   )
 }
 
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,9 @@
+function PostForm() {
+  return (
+    <div className='text-4xl'>
+      PostForm
+    </div>
+  )
+}
+
+export default PostForm
